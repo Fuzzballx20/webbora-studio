25b5b36326b8f28493258dffbd777753668e8859
Fuzzballx20 <67350708+Fuzzballx20@users.noreply.github.com>
2026-05-12 18:20:10 +0200

    trying the config thing again

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
   tanstackStart: {
     server: {
       entry: "server",
+    },
+  },
+
+  vite: {
+    server: {
       allowedHosts: ["webbora-studio.onrender.com"],
     },
   },
